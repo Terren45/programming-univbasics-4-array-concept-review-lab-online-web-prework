bda3120b377cbaa0f326158a32f4e7ee04ec8a80
Terren Culberson <culbersonterren@yahoo.com>
2019-11-14 04:16:12 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def find_element_index(|hotdog|)
+ index = hotdog 
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value([2,3,11,4,9])
+ find_max_value => 11
 end
 
-def find_min_value(array)
-  # Add your solution here
+def find_min_value([2,3,11,4,9])
+  find_min_value => 2
 end
