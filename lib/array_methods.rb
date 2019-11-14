2349765780d22467db70c983b0fc5ce6bd996fea
Terren Culberson <culbersonterren@yahoo.com>
2019-11-14 04:02:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,8 +2,8 @@ def find_element_index(|hotdog|)
  index = hotdog 
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value([2,3,11,4,9])
+  find_max_value = 11
 end
 
 def find_min_value(array)
